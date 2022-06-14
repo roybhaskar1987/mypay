410a9e15d99801397ee7bce07a632c24aac5b975
di.garg1 <divyansh.garg@geminisolutions.com>
2022-06-14 13:56:45 +0530

    Commit

--- myTeachingDemo2/src/main/java/com/example/demo/controller/UserMasterController.java
+++ myTeachingDemo2/src/main/java/com/example/demo/controller/UserMasterController.java
@@ -6,10 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.dao.UserMasterDao;
@@ -32,6 +29,11 @@ public class UserMasterController {
 	public List<UserMasterVo> getAllUsers(){
 		
 		return userMasterDemoDao.findAll() ;
+	}
+@GetMapping("/home")
+public String printMyName()
+	{
+		return "Divyansh";
 	}
    
   
